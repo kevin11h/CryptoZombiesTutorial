d4c141db3ca42b0dc39b006bed96555153e4ff08
Kevin Hung <kevhung11@gmail.com>
2018-08-18 15:56:05 -0700

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -2,9 +2,14 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
-    uint dnaDigits = 16; // non-negative integer, state variable 
-    //start here
+    uint dnaDigits = 16; // non-negative integer, state variable
+    uint dnaModulus = 10 ** dnaDigits; 
 
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 
+    // start here
 
 }
